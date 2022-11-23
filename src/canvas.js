03d8806cd8abf0c1c20d02bdb909392f97389dc4
CHOGANGYEOL <loeygnak@gmail.com>
2022-11-23 17:27:43 +0900

    캔버스 흰색 에러

--- src/canvas.js
+++ src/canvas.js
@@ -13,57 +13,60 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 ctx2.strokeStyle = "black";
+ctx2.lineWidth = 2.5;
 black.onclick = function (e) {
     ctx2.strokeStyle = "black";
+    ctx2.lineWidth = 2.5;
 }
 red.onclick = function (e) {
     ctx2.strokeStyle = "red";
+    ctx2.lineWidth = 2.5;
 }
 green.onclick = function (e) {
     ctx2.strokeStyle = "green";
+    ctx2.lineWidth = 2.5;
 }
 blue.onclick = function (e) {
     ctx2.strokeStyle = "blue";
+    ctx2.lineWidth = 2.5;
 }
 yellow.onclick = function (e) {
     ctx2.strokeStyle = "yellow";
+    ctx2.lineWidth = 2.5;
 }
 deleteBtn.onclick = function (e) {
-    if(!removeState) {
-        removeState = true;
-        // console.log(removeState)
-      } else {
-        removeState = false;
-        // console.log(removeState)
-    
-      }
+    // if(!removeState) {
+    //     removeState = true;
+    //     // console.log(removeState)
+    //   } else {
+    //     removeState = false;
+    //     // console.log(removeState)
+
+    //   }
+    ctx2.strokeStyle = "white";
+    ctx2.lineWidth = 10;
 }
-ctx2.lineWidth = 2.5;
 
 let painting = false;
 
 function startPainting() {
-    painting=true;
+    painting = true;
 }
 function stopPainting(event) {
-    painting=false;
+    painting = false;
 }
 
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
-    if(!removeState) {
-        if(!painting) {
-            ctx2.beginPath();
-            ctx2.moveTo(x, y);
-        }
-        else {
-            ctx2.lineTo(x, y);
-            ctx2.stroke();
-        }
-      } else {
-        ctx2.clearRect(x, y, 50, 50)
-      }
+    if (!painting) {
+        ctx2.beginPath();
+        ctx2.moveTo(x, y);
+    }
+    else {
+        ctx2.lineTo(x, y);
+        ctx2.stroke();
+    }
 }
 
 if (canvas) {
